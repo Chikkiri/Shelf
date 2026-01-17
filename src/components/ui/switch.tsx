abedc56f147889567a831d70f2202ec5402236d3
Chikkiri <appuz001059@gmail.com>
2026-01-17 14:35:46 +0530

    v6.1

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -9,8 +9,8 @@ interface SwitchProps extends React.ComponentPropsWithoutRef<typeof SwitchPrimit
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
-SwitchProps
->(({ className, neutral, ...props }, ref) => (  
+  SwitchProps
+>(({ className, neutral, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
       "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
